caf45af4134d4f771dc3ce4ad6ad3fa940978074
radelicroberto <44963722+radelicroberto@users.noreply.github.com>
2018-11-12 09:07:38 +0100

    Add files via upload

---
+++ jelovnik.py
@@ -0,0 +1,15 @@
+print "Welcome to the TODO task managment program "
+
+menu_dict = {}
+
+while True:
+    dish = raw_input("Please enter a TODO task: "(
+    print "Your task is:" + task
+    todo_list.append(task)
+    new = raw_input("Would you like to enter a new task (yes/no) ")
+
+    if new == "no":
+        break
+
+
+print "END"
